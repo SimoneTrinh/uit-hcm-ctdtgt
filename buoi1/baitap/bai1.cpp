bd54041acb6e9c14927e1aa4ec84a4b21ab265e6
Simone Trinh <trinhthelong.bav@gmail.com>
2024-06-21 15:08:57 +0700

    create random persudo bai tap 1

--- buoi1/baitap/bai1.cpp
+++ buoi1/baitap/bai1.cpp
@@ -1,44 +1,124 @@
 #include <stdio.h>
 #include <iostream>
+#include <math.h>
+#include <string>
 
 using namespace std;
 
+int *seed = new int();
+
 int getRandomNumber()
 {
-    int *a = new int;
-    delete a;
-    return *a;
+    int *result = new int(0);
+    delete result;
+    return *result;
 }
 
-int hoanvi(int *a, int *b)
+int getRandomPositiveNumber()
 {
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-    return 0;
+    int result = getRandomNumber();
+    if (result < 0)
+    {
+        result = -result;
+    }
+    return result;
+}
+
+long long multiBy10(int multipler)
+{
+    long long result = 1;
+    if (multipler == 0)
+    {
+        result = result;
+    }
+    else if (multipler > 0)
+    {
+        for (int i = 0; i < multipler; i++)
+        {
+            result *= 10;
+        }
+    }
+    return result;
+}
+
+int countDigitInNumber(int nInput)
+{
+    if (nInput < 10 && nInput >= 0)
+    {
+        return 1;
+    }
+    else if (nInput < 0)
+    {
+        nInput = -(nInput);
+    }
+    return 1 + countDigitInNumber(nInput / 10);
+}
+
+int extractXDigitFromNumber(int number, int digit)
+{
+    string numChar = to_string(number);
+    int startPos = int(numChar.length() - digit) / 2;
+    string middleDigit = numChar.substr(startPos, digit);
+    return stoi(middleDigit);
+}
+
+void setBaseSeed()
+{
+    int baseSeed = getRandomPositiveNumber();
+    int maxDigitSeed = countDigitInNumber(baseSeed);
+    long long baseNumber = multiBy10(maxDigitSeed);
+    int base4Seed = (float(baseSeed) / baseNumber) * (9999 - 1000 + 1) + 1000;
+    *seed = base4Seed;
+}
+
+float getRandomSeedPersudo(int &seed)
+{
+    int sqrtSeed = seed * seed;
+    int maxDigitSeed = countDigitInNumber(sqrtSeed);
+    long long baseNumber = multiBy10(maxDigitSeed);
+    seed = extractXDigitFromNumber(sqrtSeed, 4);
+    return float(sqrtSeed) / baseNumber;
+}
+
+float roundToTwoDecimal(float value)
+{
+    return floor(value * 100 + 0.5) / 100;
+}
+
+int getRandomBetween(int minNum, int maxNum)
+{
+    return getRandomSeedPersudo(*seed) * (maxNum - minNum + 1) + minNum;
+}
+
+float getRandomFloatBetween(int minNum, int maxNum)
+{
+    return roundToTwoDecimal(getRandomSeedPersudo(*seed) * (maxNum - minNum + 1) + minNum);
+}
+
+void initDynamicArr(float *array, int &length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        *(array + i) = getRandomFloatBetween(268, 339);
+    }
+}
+
+void printArray(float *array, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        cout << *(array + i) << endl;
+    }
 }
 
 int main()
 {
-    int arr[] = {32, 13, 66, 11, 22};
-    int *randomArr = new int[10];
-    // int *a = new int();
-    // delete a; // value of a is random number
-    // cout << (*a) % 10;
-    // int z = 10;
-    // int *t = new int(10);
-    // count << *z;
-    // count <<
-    // int *a = new int(1);
+    // 1. Tao gia tri ngau nhien (so thuc 2 chu so sau dau cham) thuoc (268; 339). SL ngau nhien thuoc [10;20]
+    setBaseSeed();
+    int arrayLength = getRandomBetween(10, 20);
+    float *randomArr = new float[arrayLength];
+    initDynamicArr(randomArr, arrayLength);
 
-    // cout << getRandomNumber() << endl;
-    // cout << *a;
-
-    int a = 3;
-    int b = 4;
-    cout << *(&a) << endl;
-    hoanvi(&a, &b);
-    cout << a << endl;
-    cout << b << endl;
+    // 2. In mang vua tao
+    printArray(randomArr, arrayLength);
 }
